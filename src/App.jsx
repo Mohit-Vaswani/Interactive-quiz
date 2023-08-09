04efc23bd9baf6b8f14871c29c4437abd3b1999a
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-08-09 10:50:56 +0530

    some basic updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Hero/>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="w-full h-screen bg-orange-600 relative overflow-hidden">
+      <h1 className="text-9xl font-sans absolute bottom-0 right-0">Quiz</h1>
+    </section>
+  )
+}
+
+export default Hero
