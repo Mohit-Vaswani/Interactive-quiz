efa7dc61382ece29406415fde0f357d8229dc3e4
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-08-12 10:54:51 +0530

    some updates

--- src/components/QuizModal.jsx
+++ src/components/QuizModal.jsx
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types';
+import { ImCross } from 'react-icons/im'
+import { GoTrophy } from 'react-icons/go'
 
 const QuizModal = ({ modalActive, closeModal }) => {
     return (
-      <div className="main-modal" style={{ display: modalActive ? 'flex' : 'none' }}>
-        <div className="quiz_modal p-10">
-          {/* Modal content */}
-          <h2>Quiz Modal Content</h2>
-          <p>This is some quiz-related content.</p>
-          <button onClick={closeModal}>Close</button>
+      <div className="main-modal bg-white" style={{ display: modalActive ? 'flex' : 'none' }}>
+        <div className="quiz_modal p-10 relative flex justify-center items-center flex-col gap-10">
+          <span className='border-2 p-6 border-black rounded-md'><GoTrophy fontSize={'30px'}/></span>
+          <h2 className='text-5xl font-semibold'>Now Game On</h2>
+          <p className='text-xl font-medium'>Answer 5 simple questions to win the quiz</p>
+          <button onClick={closeModal} className='text-white rounded-full px-8 py-3 text-xl font-medium bg-gray-700 hover:bg-gray-800 transition-all duration-500 flex gap-3 items-center'> <span><GoTrophy fontSize={'20px'}/></span> Let's take the quiz</button>
+          <button onClick={closeModal} className='absolute top-0 right-0 m-3'><ImCross/></button>
         </div>
       </div>
     );
---
+++ src/components/QuizOne.jsx
@@ -0,0 +1,9 @@
+const QuizOne = () => {
+  return (
+    <div className="quiz_modal p-10 relative flex justify-center items-center flex-col gap-10">
+        QuizOne
+    </div>
+  )
+}
+
+export default QuizOne
